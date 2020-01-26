6ae44cd1781742b9eee0cb5c19bdaa83c7e4cbb3
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2020-01-26 17:06:07 +0800

    Update view of login page

--- app/javascript/components/LoginForm.js
+++ app/javascript/components/LoginForm.js
@@ -2,19 +2,69 @@ import React from "react"
 import { Row, Col } from 'antd'
 import PropTypes from "prop-types"
 import { blue } from '@ant-design/colors'
+import { Layout, Menu, Form, Icon, Input, Button, Checkbox } from 'antd';
+const { Header, Content, Footer } = Layout;
+
 class LoginForm extends React.Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
   render () {
+    const { getFieldDecorator } = this.props.form;
     return (
       <React.Fragment>
-        <div>
-          <Row>
-            <Col span={12} offset={5} style={{ backgroundColor: blue.primary }}>
-            </Col>
-          </Row>
-        </div>
+        <Layout className="layout">
+          <Content style={{ padding: '0 50px' }}>
+            <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+              <Form onSubmit={this.handleSubmit} className="login-form">
+                <Form.Item>
+                  {getFieldDecorator('email', {
+                    rules: [{ required: true, message: 'Please input your email!' }],
+                  })(
+                    <Input
+                      prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                      placeholder="Email"
+                    />,
+                  )}
+                </Form.Item>
+                <Form.Item>
+                  {getFieldDecorator('password', {
+                    rules: [{ required: true, message: 'Please input your Password!' }],
+                  })(
+                    <Input
+                      prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                      type="password"
+                      placeholder="Password"
+                    />,
+                  )}
+                </Form.Item>
+                <Form.Item>
+                  {getFieldDecorator('remember', {
+                    valuePropName: 'checked',
+                    initialValue: true,
+                  })(<Checkbox>Remember me</Checkbox>)}
+                  <a className="login-form-forgot" href="">
+                    Forgot password
+                  </a>
+                  <Button type="primary" htmlType="submit" className="login-form-button">
+                    Log in
+                  </Button>
+                  Or <a href="">register now!</a>
+                </Form.Item>
+              </Form>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
       </React.Fragment>
     );
   }
 }
 
-export default LoginForm
+export default Form.create()(LoginForm);
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,3 +20,6 @@ require("antd/dist/antd.css")
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
+function hasErrors(fieldsError) {
+  return Object.keys(fieldsError).some(field => fieldsError[field]);
+}
